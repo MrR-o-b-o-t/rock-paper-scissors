addfd32e53a7c456753fb60c4b6d188887091a33
Sam Kautz <samkautz@yahoo.com>
2020-02-22 20:17:01 -0600

    Update scoreTracker function.

--- scripts.js
+++ scripts.js
@@ -3,9 +3,6 @@ let x = document.getElementById('rock').addEventListener('click', userChoice);
 let y = document.getElementById('paper').addEventListener('click', userChoice);
 let z = document.getElementById('scissors').addEventListener('click', userChoice);
 
-// let computerChoice;
-// let playerChoice;
-
 let computerScore = 0;
 let playerScore = 0;
 
@@ -17,40 +14,41 @@ function userChoice() {
 }
 
 function computerChoiceFunction() {
-	let computerChoice = Math.ceil(Math.random() * 3);
-	if (computerChoice === 1) {
+	let randomNumber = Math.ceil(Math.random() * 3);
+	if (randomNumber === 1) {
 		computerChoice = 'rock';
-	} else if (computerChoice === 2) {
+	} else if (randomNumber === 2) {
 		computerChoice = 'paper';
 	} else {
 		computerChoice = 'scissors';
 	}
+	console.log(randomNumber)
+	console.log(computerChoice)
 	return computerChoice
 }
 
 function compareChoices() {
+	console.log(computerChoice)
 
-	let thisComputerChoice = computerChoiceFunction()
-
-	if (thisComputerChoice === playerChoice) {
+	if (computerChoice === playerChoice) {
 		document.getElementById('computer-message').innerHTML =
-			'You both chose ' + thisComputerChoice + '! Its a draw!';
-	} else if (thisComputerChoice === 'paper' && playerChoice === 'scissors') {
+			'You both chose ' + computerChoice + '! Its a draw!';
+	} else if (computerChoice === 'paper' && playerChoice === 'scissors') {
 		document.getElementById('computer-message').innerHTML =
 			'The computer chose paper! You chose ' + playerChoice + '. You win!';
-	} else if (thisComputerChoice === 'paper' && playerChoice === 'rock') {
+	} else if (computerChoice === 'paper' && playerChoice === 'rock') {
 		document.getElementById('computer-message').innerHTML =
 			'Computer chose paper! You chose Rock! You lose!';
-	} else if (thisComputerChoice === 'rock' && playerChoice === 'paper') {
+	} else if (computerChoice === 'rock' && playerChoice === 'paper') {
 		document.getElementById('computer-message').innerHTML =
 			'Computer chose rock! You chose paper! You win!';
-	} else if (thisComputerChoice === 'rock' && playerChoice === 'scissors') {
+	} else if (computerChoice === 'rock' && playerChoice === 'scissors') {
 		document.getElementById('computer-message').innerHTML =
 			'Computer chose rock! You chose Scissors! You lose!';
-	} else if (thisComputerChoice === 'scissors' && playerChoice === 'rock') {
+	} else if (computerChoice === 'scissors' && playerChoice === 'rock') {
 		document.getElementById('computer-message').innerHTML =
 			'Computer chose scissors! You chose rock! You win!';
-	} else if (thisComputerChoice === 'scissors' && playerChoice === 'paper') {
+	} else if (computerChoice === 'scissors' && playerChoice === 'paper') {
 		document.getElementById('computer-message').innerHTML =
 			'Computer chose scissors!' + ' You chose paper!' + ' You lose!';
 	}
@@ -58,20 +56,25 @@ function compareChoices() {
 
 function scoreTracker() {
 
-	let thisComputerChoice = computerChoiceFunction()
-
-	if (thisComputerChoice === 'paper' && playerChoice === 'scissors') {
-		playerScore += 1
-	} else if (thisComputerChoice === 'paper' && playerChoice === 'rock') {
-		computerScore += 1
-	} else if (thisComputerChoice === 'scissors' && playerChoice === 'rock') {
-		playerScore += 1
-	} else if (thisComputerChoice === 'scissors' && playerChoice === 'paper') {
-		computerScore += 1
-	} else if (thisComputerChoice === 'rock' && playerChoice === 'paper') {
-		playerScore += 1
-	} else if (thisComputerChoice === 'rock' && playerChoice === 'scissors') {
-		computerScore += 1
+	console.log(computerChoice)
+	if (computerChoice === 'paper' && playerChoice === 'scissors') {
+		playerScore++ , computerScore + 0
+	} else if (computerChoice === 'paper' && playerChoice === 'rock') {
+		computerScore++ , playerScore + 0
+	} else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+		playerScore++ , computerScore + 0
+	} else if (computerChoice === 'scissors' && playerChoice === 'paper') {
+		computerScore++ , playerScore + 0
+	} else if (computerChoice === 'rock' && playerChoice === 'paper') {
+		playerScore++ , computerScore + 0
+	} else if (computerChoice === 'rock' && playerChoice === 'scissors') {
+		computerScore++ , playerScore + 0
+	} else if (computerChoice === 'scissors' && playerChoice === 'scissors') {
+		computerScore + 0, playerScore + 0
+	} else if (computerChoice === 'rock' && playerChoice === 'rock') {
+		computerScore + 0, playerScore + 0
+	} else if (computerChoice === 'paper' && playerChoice === 'paper') {
+		computerScore + 0, playerScore + 0
 	}
 	document.getElementById('computers-score').innerText = 'The Computers score is ' + computerScore
 	document.getElementById('players-score').innerText = 'Your score is ' + playerScore
